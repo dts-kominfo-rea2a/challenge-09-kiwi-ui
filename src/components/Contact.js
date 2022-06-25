c729ad0bcff86c6b58579903e588beacd89dae12
kiwi-ui <yusnianggara@gmail.com>
2022-06-25 21:53:19 +0700

    bismilah

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,9 +2,19 @@
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 import React from 'react';
 
-const Contact = () => {
+const Contact = ({ props }) => {
     return (
-        <></>
+        <>
+        <div className='card'>
+            <img src='https://placekitten.com/200/200' alt='ini foto kucing' className='contact-image'></img>        
+                <div className='contact-info'>
+                    <h3 className='contact-name'>{props.name}</h3>
+                    <h3 className='contact-phone'>{props.phone}</h3>
+                    <h3 className='contact-email'>{props.email}</h3>
+
+            </div>
+            </div>
+        </>
     )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 // Kalian bisa menambahkan CSS di src/components/Header.css
 import React from 'react';
-
 const Header = () => {
     return (
-        <></>
+        <div className='header'>
+            <h1 className='header-title'>Call a Friend</h1>
+            <h5 className='header-subtitle'>Your friendly contact app</h5>
+        </div>
     )
 }
 
